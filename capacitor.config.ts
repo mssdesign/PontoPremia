239805f3b0679e7329d3ee7b456695adafbe2d1c
Matheus Soares <matheussilvasoares43@gmail.com>
2024-02-29 17:13:22 -0300

    Alteração de nome

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'fideliza-agro-front',
+  appName: 'pontoPremia-front',
   webDir: 'www',
   server: {
     androidScheme: 'https'
